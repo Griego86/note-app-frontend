cd0312e998accef62e9496dbfa968d441363a096
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-16 17:33:14 -0700

    Add sample notes to render on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 
-function App() {
+const App = ({notes}) => {
   return (
     <div>
-      Helo World
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+      </ul>
     </div>
   )
 }
