810747ebfa853ea1f55dbe54617e8c623b94eb0c
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-23 14:40:07 -0700

    Add function to show username when logged in

--- src/App.jsx
+++ src/App.jsx
@@ -117,7 +117,13 @@ const App = () => {
       <h1>Notes</h1>
       <Notification message={errorMessage} />
 
-      {user === null ? loginForm() : noteForm()}
+      {user === null ? 
+        loginForm() : 
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+        }
 
       <div>
         <button onClick={() => setShowAll(!showAll)}>
